48c89a6b3bd01c9ed32a1a451ac7407e56c8c58f
Gabriel Amancio <38165233+JecaTatu@users.noreply.github.com>
2018-12-06 15:25:58 -0300

    Função para separar os alunos por turma, curso, etc

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser');
 // ==================== GLOBAL VARIABLES ==================== //
 
 const app = express();
+const alunos = []; 
 
 
 // ==================== MIDDLEWARE ==================== //
@@ -28,13 +29,18 @@ app.use(bodyParser.json());
 
 
 // ==================== FUNCTIONS ==================== //
-
+// função para separar alunos por colunas da planilha, caso não seja selecionado nada, mostrará todos.
+app.get('/api/alunos', (req, res) =>{
+    if(!req.query.column){
+        res.send(alunos);
+        return;
+    }
+    res.send(alunos.map(aluno => aluno[req.query.column]));
+});
 // ==================== ROUTES ==================== //
-let alunos;
-
+// request que alimenta o array com os alunos cadastrados via uma planilha
 request("https://spreadsheets.google.com/feeds/list/1HR2AXgWv9DXqqe7SvmU8MkSp_cbQqYOTUT2Qb-7vFSE/od6/public/values?alt=json", (err, res) => {
     const data = JSON.parse(res.body);
-    const alunos = []; 
     for(i = 0; i < data.feed.entry.length; i++){
         alunos.push({
             aluno: data.feed.entry[i]['gsx$aluno']['$t'],
